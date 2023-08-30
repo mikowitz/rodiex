57edbd87b7d7f641fc07543b92e153299ca1db05
Michael berkowitz <michael.berkowitz@gmail.com>
2023-08-30 13:49:58 -0400

    mix new rodiex

---
+++ lib/rodiex.ex
@@ -0,0 +1,18 @@
+defmodule Rodiex do
+  @moduledoc """
+  Documentation for `Rodiex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rodiex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rodiex_test.exs
@@ -0,0 +1,8 @@
+defmodule RodiexTest do
+  use ExUnit.Case
+  doctest Rodiex
+
+  test "greets the world" do
+    assert Rodiex.hello() == :world
+  end
+end

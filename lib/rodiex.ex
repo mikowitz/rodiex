0eeb9585cada87a38fc9a6f6213c7c3adf5d353b
Michael berkowitz <michael.berkowitz@gmail.com>
2023-08-31 09:20:08 -0400

    Add ability to play single or multiple frequencies as sine waves

--- lib/rodiex.ex
+++ lib/rodiex.ex
@@ -1,2 +1,21 @@
 defmodule Rodiex do
+  alias Rodiex.Native, as: N
+
+  def play(freq, opts \\ []) when is_number(freq) do
+    duration = Keyword.get(opts, :duration, 2.0)
+
+    N.play(freq / 1, duration / 1)
+
+    freq
+  end
+
+  def play_chord(freqs, opts \\ []) when is_list(freqs) do
+    duration = Keyword.get(opts, :duration, 2.0)
+
+    freqs
+    |> Enum.map(&(&1 / 1))
+    |> N.play_chord(duration / 1)
+
+    freqs
+  end
 end
--- lib/rodiex/native.ex
+++ lib/rodiex/native.ex
@@ -1,5 +1,9 @@
 defmodule Rodiex.Native do
   use Rustler, otp_app: :rodiex, crate: "rodiex"
 
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def play(_freq, _duration), do: nif_error()
+
+  def play_chord(_freqs, _duration), do: nif_error()
+
+  defp nif_error, do: :erlang.nif_error(:nif_not_loaded)
 end

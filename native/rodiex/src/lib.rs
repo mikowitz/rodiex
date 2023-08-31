a57e394946e11155537facda5841cbb5a5dd8c0a
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-08-31 12:38:46 -0400

    Threading caused some downstream annoyances I don't want to figure out at the moment

--- native/rodiex/src/lib.rs
+++ native/rodiex/src/lib.rs
@@ -2,41 +2,36 @@ use rodio::{
     source::{Amplify, SineWave, Source, TakeDuration},
     OutputStream, Sink,
 };
-use std::thread;
 use std::time::Duration;
 
 #[rustler::nif]
 fn play(freq: f32, duration: f32) {
-    thread::spawn(move || {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
 
-        let sine_wave = create_sine_wave(freq, duration, 0.2);
+    let sine_wave = create_sine_wave(freq, duration, 0.2);
 
-        sink.append(sine_wave);
-        sink.sleep_until_end()
-    });
+    sink.append(sine_wave);
+    sink.sleep_until_end()
 }
 
 #[rustler::nif]
 fn play_chord(freqs: Vec<f32>, duration: f32) {
-    thread::spawn(move || {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-        let mut sinks = vec![];
+    let mut sinks = vec![];
 
-        let amp = 1. / freqs.len() as f32;
+    let amp = 1. / freqs.len() as f32;
 
-        for freq in freqs {
-            let sink = Sink::try_new(&stream_handle).unwrap();
-            sink.append(create_sine_wave(freq, duration, amp));
-            sinks.push(sink);
-        }
+    for freq in freqs {
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        sink.append(create_sine_wave(freq, duration, amp));
+        sinks.push(sink);
+    }
 
-        for sink in sinks {
-            sink.sleep_until_end();
-        }
-    });
+    for sink in sinks {
+        sink.sleep_until_end();
+    }
 }
 
 pub(crate) fn create_sine_wave(

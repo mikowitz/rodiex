98297da6e8588eb38093335c1426767fe80a8486
Michael berkowitz <michael.berkowitz@gmail.com>
2023-09-20 11:51:03 -0400

    Extract logic into `SineWave` structs

--- lib/rodiex.ex
+++ lib/rodiex.ex
@@ -1,23 +1,5 @@
 defmodule Rodiex do
-  alias Rodiex.Native, as: N
-
-  def play(freq_or_freqs, opts \\ [])
-
-  def play(freq, opts) when is_number(freq) do
-    duration = Keyword.get(opts, :duration, 2.0)
-
-    N.play(freq / 1, duration / 1)
-
-    freq
-  end
-
-  def play(freqs, opts) when is_list(freqs) do
-    duration = Keyword.get(opts, :duration, 2.0)
-
-    freqs
-    |> Enum.map(&(&1 / 1))
-    |> N.play_chord(duration / 1)
-
-    freqs
+  def play(source, opts \\ []) do
+    Rodiex.Play.play(source, opts)
   end
 end
